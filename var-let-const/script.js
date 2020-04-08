3a2d1a799b515353bc095904250b7d2ff378b17b
maclararose <maria.uespi@outlook.com>
2020-04-08 17:46:51 -0300

    Var-let-const

---
+++ var-let-const/script.js
@@ -0,0 +1,20 @@
+//creating a VARIABLE
+
+var name = "Maria";
+
+//creating a condicion
+
+if(name == 'Maria'){
+    let lastName = "Barroso";
+    console.log("Maria "+lastName);
+}
+
+// LET is temporary and can only be accessed in the scope
+
+// creating a CONST
+
+const name2 = "Matheus";
+
+console.log(name2);
+
+name2 = "Marcos"; // Error, because CONST is immutable
